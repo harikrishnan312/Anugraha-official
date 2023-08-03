7a78586ac179e6bdd0c436eef1d28b28e53f6836
Harikrishnanks312 <118618866+Harikrishnanks312@users.noreply.github.com>
2023-08-03 13:41:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000; // You can change this port if needed
+
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
